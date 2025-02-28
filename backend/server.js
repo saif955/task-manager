20eee0812799116d098a4a98b94112a45714d242
saif955 <saif.rahman.95595@gmail.com>
2025-02-28 12:08:18 +0600

    .env

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,9 @@
-console.log('hello world');
+import express from 'express';
+import dotenv from 'dotenv';
+const port= process.env.PORT||5000;
+
+const app = express();
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
